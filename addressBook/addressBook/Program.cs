8e1ac8be70cb663d5f12aa08c9524a3353e47452
CMD <gautamkailashsingh@gmail.com>
2020-10-01 14:57:43 +0530

    Added a feature to create contacts.

--- addressBook/addressBook/Program.cs
+++ addressBook/addressBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.ExceptionServices;
 
 namespace addressBook
 {
@@ -7,6 +8,9 @@ namespace addressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Address Book Program!");
+
+            var varContact = new contact("First", "Last", "Address", "City", "State", "Phone", "Email", 100050);
+            Console.WriteLine(varContact.FirstName + "--" + varContact.LastName + "--" + varContact.Address + "--" + varContact.City + "--" + varContact.State + "--" + varContact.Zip + "--" + varContact.Phone + "--" + varContact.Email);
         }
     }
 }
---
+++ addressBook/addressBook/contact.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace addressBook
+{
+    public class contact
+    {
+        string first = "", last = "", add = "", city = "", state = "", phone = "", email = "";
+        int zip = 0;
+
+        public contact(string first, string last, string add, string city, string state, string phone, string email, int zip)
+        {
+            this.first = first;
+            this.last = last;
+            this.add = add;
+            this.city = city;
+            this.state = state;
+            this.phone = phone;
+            this.email = email;
+            this.zip = zip;
+        }
+
+        public string FirstName
+        { get=>first; set=>first=value; }
+
+        public string LastName
+        { get=>last; set=>last=value; }
+
+        public string Address
+        { get=>add; set=>add=value; }
+
+        public string City
+        { get=>city; set=>city=value; }
+
+        public string State
+        { get=>state; set=>state=value; }
+
+        public int Zip
+        { get=>zip; set=>zip=value; }
+
+        public string Phone
+        { get=>phone; set=>phone=value; }
+
+        public string Email
+        { get=>email; set=>email=value; }
+
+    }
+}
