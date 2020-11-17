921bd5620df30aa8838a43e16fbee9a5a5183436
CMD <gautamkailashsingh@gmail.com>
2020-11-17 20:42:11 +0530

    [Gautam] Refactored the code.

--- addressBook/JSONServerTest/UnitTest1.cs
+++ addressBook/JSONServerTest/UnitTest1.cs
@@ -7,43 +7,75 @@ using System.Collections.Generic;
 
 namespace JSONServerTest
 {
+    /// <summary>
+    /// The class is written to store the contacts of a person
+    /// </summary>
     public class Contact
     {
+        /// <summary>
+        /// A property to store the id of a contact
+        /// </summary>
         public int ID
         { get; set; }
+        /// <summary>
+        /// A property to store the name of a contact
+        /// </summary>
         public string Name
         { get; set; }
-
+        /// <summary>
+        /// A property to store the address of a contact
+        /// </summary>
         public string Address
         { get; set; }
-
+        /// <summary>
+        /// A property to store the city of a contact
+        /// </summary>
         public string City
         { get; set; }
-
+        /// <summary>
+        /// A property to store the state of a contact
+        /// </summary>
         public string State
         { get; set; }
-
+        /// <summary>
+        /// A property to store the zip of a contact
+        /// </summary>
         public int Zip
         { get; set; }
-
+        /// <summary>
+        /// A property to store the phone of a contact
+        /// </summary>
         public string Phone
         { get; set; }
-
+        /// <summary>
+        /// A property to store the email of a contact
+        /// </summary>
         public string Email
         { get; set; }
 
     }
+
+    /// <summary>
+    /// A class to test the address book json server
+    /// </summary>
     [TestClass]
     public class UnitTest1
     {
         RestClient client;
 
+        /// <summary>
+        /// A method to initialize the restclient object with the json server address
+        /// </summary>
         [TestInitialize]
         public void SetUp()
         {
             client = new RestClient("http://localhost:3000");
         }
 
+        /// <summary>
+        /// This function is used to request the json server to send the data stored
+        /// </summary>
+        /// <returns>An IRestResponse object that contains the response sent by the json server</returns>
         private IRestResponse getContactList()
         {
             RestRequest request = new RestRequest("/contacts", Method.GET);
@@ -51,17 +83,23 @@ namespace JSONServerTest
             return response;
         }
 
+        /// <summary>
+        /// This test method gets the data stored in the server and prints the output
+        /// </summary>
         [TestMethod]
         public void OnClaiingGETApi_ReturnContactList()
         {
             IRestResponse response = getContactList();
             Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.OK);
             List<Contact> dataresponse = JsonConvert.DeserializeObject<List<Contact>>(response.Content);
-            Assert.AreEqual(3, dataresponse.Count);
+            Assert.AreEqual(5, dataresponse.Count);
             foreach (var item in dataresponse)
-                System.Console.WriteLine("ID: "+item.ID+" Name: "+item.Name+" Address: "+item.Address+" City: "+item.City+" State: "+item.State+" Zip: "+item.Zip+" Phone: "+item.Phone+" Email: "+item.Email );
-            }
+                System.Console.WriteLine("ID: " + item.ID + " Name: " + item.Name + " Address: " + item.Address + " City: " + item.City + " State: " + item.State + " Zip: " + item.Zip + " Phone: " + item.Phone + " Email: " + item.Email);
+        }
 
+        /// <summary>
+        /// This test method "POST's" the data into the json server
+        /// </summary>
         [TestMethod]
         public void GivenMultipleContacts_OnPost_ShouldReturnCountOfContacts()
         {
@@ -99,6 +137,9 @@ namespace JSONServerTest
             }
         }
 
+        /// <summary>
+        /// This test method updates a contact's name based on the id
+        /// </summary>
         [TestMethod]
         public void GivenEmployee_OnPatch_ShouldReturnUpdatedEmployee()
         {
